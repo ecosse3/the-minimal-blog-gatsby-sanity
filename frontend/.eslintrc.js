95c28795174819bf8a494fbee653fceeb892575c
ecosse3 <theecosse@gmail.com>
2020-09-15 01:05:43 +0200

    config(eslint): set unused vars as warn

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
   plugins: ["react"],
   rules: {
     "linebreak-style": ["error", "unix"],
+    "no-unused-vars": ["warn"],
     "react/no-unescaped-entities": ["warn"],
   },
 };
