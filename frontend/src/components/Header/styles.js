b891f68f582950b983c55cc29d6a471fc3e11bc3
ecosse3 <theecosse@gmail.com>
2020-09-15 01:06:38 +0200

    feat(header): add grid system to header

---
+++ frontend/src/components/Header/styles.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+import { Link } from "gatsby";
+
+export const HeaderContainer = styled.header`
+  background: transparent;
+  font-family: "Poppins";
+  color: #000000;
+  margin-bottom: 1.45rem;
+  padding-top: 1.45rem;
+  padding-bottom: 1.45rem;
+`;
+
+export const Redirect = styled(Link)`
+  color: #000000;
+  text-decoration: none;
+`;
+
+export const Title = styled.h2`
+  color: #222222;
+  font-size: 1.25rem;
+  font-weight: 600;
+`;
