23cd400f0202b124f270176a9d545dc2e7e540b8
ecosse3 <theecosse@gmail.com>
2020-09-16 01:28:28 +0200

    feat(layout): cleave site title

--- frontend/src/components/Layout/index.js
+++ frontend/src/components/Layout/index.js
@@ -1,10 +1,3 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react";
 import PropTypes from "prop-types";
 import { useStaticQuery, graphql } from "gatsby";
@@ -28,10 +21,12 @@ const Layout = ({ children }) => {
     }
   `);
 
+  const siteTitle = data.site.siteMetadata.title;
+
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Header siteTitle={data.site.siteMetadata.title} />
+      <Header siteTitle={siteTitle} />
       <Grid>
         <main>{children}</main>
       </Grid>
