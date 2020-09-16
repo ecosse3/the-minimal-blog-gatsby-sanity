7a84a7e86559a11cce256a47301f46ba8444ed17
ecosse3 <theecosse@gmail.com>
2020-09-16 21:52:58 +0200

    feat(backend): add pageBox schema

---
+++ studio/schemas/pageBox.js
@@ -0,0 +1,27 @@
+export default {
+  name: 'pageBox',
+  title: 'Page Box',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string'
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text'
+    },
+    {
+      name: 'link',
+      title: 'Link',
+      type: 'string'
+    },
+    {
+      name: 'buttonText',
+      title: 'Button Text',
+      type: 'string'
+    }
+  ]
+}
